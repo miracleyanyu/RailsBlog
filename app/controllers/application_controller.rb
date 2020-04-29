100fe550fdcd00b86b3f98749f06eafd6e1d3288
Yu Yan <miracleyanyu@gmail.com>
2020-04-28 22:37:35 -0700

    Refactored application controller to include concern for source feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
 
-  before_action :set_source
+  # before_action :set_source
 
   # Can be used to implemented shopping cart
   # Session data not secure
   # No password, no credit card
-  def set_source
-    session[:source] = params[:q] if params[:q]
-  end
+  # def set_source
+  #   session[:source] = params[:q] if params[:q]
+  # end
+  include SetSource
 end
