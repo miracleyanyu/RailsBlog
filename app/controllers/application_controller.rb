f5539581cb73086bfa4fe7e06d18dcc4e1f54e8a
Yu Yan <miracleyanyu@gmail.com>
2020-04-28 22:28:44 -0700

    Added session tracker for dynamic source links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
+
+  before_action :set_source
+
+  # Can be used to implemented shopping cart
+  # Session data not secure
+  # No password, no credit card
+  def set_source
+    session[:source] = params[:q] if params[:q]
+  end
 end
