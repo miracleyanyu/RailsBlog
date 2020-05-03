afc3359bf97039d57ad6e85818bdd87c6eaf5c5f
Yu Yan <miracleyanyu@gmail.com>
2020-05-03 14:41:57 -0700

    Implemented persistant position updates for drag and drop interface

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -11,6 +11,14 @@ class PortfoliosController < ApplicationController
     @angular_portfolio_items = Portfolio.angular
   end
 
+  def sort
+    params[:order].each do |key, value|
+      Portfolio.find(value[:id]).update(position: value[:position])
+    end
+
+    render nothing: true
+  end
+
   def new
     @portfolio_item = Portfolio.new
     3.times { @portfolio_item.technologies.build }
--- app/javascript/packs/portfolios.js
+++ app/javascript/packs/portfolios.js
@@ -1,8 +1,36 @@
-// app/javascript/packs/portfolios.js
-
 require("packs/html.sortable")
 require("jquery-ui")
 
+var set_positions;
+
+set_positions = () => {
+    $('.card-item').each(function (i) {
+        $(this).attr('data-pos', i + 1);
+    });
+}
+
 $(function(){
+    set_positions();
     $('.sortable').sortable();
+    $('.sortable').sortable().bind('sortupdate', function(e, ui) {
+        var update_order;
+        update_order = []
+        set_positions();
+        $('.card-item').each(function (i) {
+            update_order.push({
+                id: $(this).data('id'),
+                position: i + 1
+            });
+        });
+        $.ajax({
+            beforeSend: (xhr) => {
+                xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))
+            },
+            type: 'PUT',
+            url: '/portfolios/sort',
+            data: {
+                order: update_order
+            }
+        });
+    });
 });
